1079c8fec5c86998fea748fd6fcf8eaaf0e27711
yobibyte <y0b1byte@proton.me>
2025-01-05 22:52:28 +0000

    One more try.

--- lua/helix/groups/statusline.lua
+++ lua/helix/groups/statusline.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.highlight(palette, opts)
   return {
-    StatusLine = { fg = palette.gray, bg = palette.bg },
+    StatusLine = { fg = palette.gray, bg = palette.revolver},
     StatusLineNC = { fg = palette.alt_bg, bg = palette.revolver},
     StatusLineSeparator = { fg = palette.bg },
     StatusLineTerm = { fg = palette.bg },
